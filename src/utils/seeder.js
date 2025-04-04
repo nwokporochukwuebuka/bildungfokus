3cebd78ca65bf28daf0fd3fcd9f493112f3382b0
Nwokporo Chukwuebuka <nwokporochukwuebuka@gmail.com>
2025-04-04 04:20:41 +0100

    second commit

--- src/utils/seeder.js
+++ src/utils/seeder.js
@@ -6,26 +6,30 @@ const gradeList = require("./grade-list");
 const subjectList = require("./subject-list");
 
 module.exports.seedSubjects = async () => {
-  subjectList.forEach(async (subject) => {
-    // check first
-    const check = await db.subject.findOne({ where: { slug: subject.slug } });
+  setTimeout(() => {
+    subjectList.forEach(async (subject) => {
+      // check first
+      const check = await db.subject.findOne({ where: { slug: subject.slug } });
 
-    if (!check) {
-      await db.subject.create({ name: subject.name, slug: subject.slug });
+      if (!check) {
+        await db.subject.create({ name: subject.name, slug: subject.slug });
+        return;
+      }
       return;
-    }
-    return;
-  });
+    });
+  }, 2000);
 };
 
 module.exports.seedGrade = async () => {
-  gradeList.forEach(async (grade) => {
-    const check = await db.grade.findOne({ where: { slug: grade.slug } });
+  setTimeout(() => {
+    gradeList.forEach(async (grade) => {
+      const check = await db.grade.findOne({ where: { slug: grade.slug } });
 
-    if (!check) {
-      await db.grade.create({ name: grade.name, slug: grade.slug });
+      if (!check) {
+        await db.grade.create({ name: grade.name, slug: grade.slug });
+        return;
+      }
       return;
-    }
-    return;
-  });
+    });
+  }, 3000);
 };
